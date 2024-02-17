8891ba7dd4e72cc736382030d1d282c2e6670986
mrz-256 <134142969+mrz-256@users.noreply.github.com>
2024-02-17 21:22:09 +0100

    Update AgentProcessor.cpp
    edited comments

--- AgentProcessor.cpp
+++ AgentProcessor.cpp
@@ -15,8 +15,9 @@ AgentProcessor::AgentProcessor(int width, int height) {
             (std::chrono::high_resolution_clock::now().time_since_epoch()).count();
     std::cout << "SEED: " << _seed << "\n";
 
+    // commented part makes agents spawn at random places
     for (int i = 0; i < AGENT_COUNT; ++i) {
-        int x = WIDTH / 2; //rnd(this->_seed++) % WIDTH;
+        int x = WIDTH  / 2; //rnd(this->_seed++) % WIDTH;
         int y = HEIGHT / 2; //rnd(this->_seed++) % HEIGHT;
         double angle = rnd(_seed++) % 10;
         agents.push_back(new Agent(sf::Vector2i(x, y), angle));
@@ -125,13 +126,15 @@ void AgentProcessor::processAgent(Agent *agent) {
 }
 
 void AgentProcessor::updateAgentWrapping(Agent *agent) {
-    std::vector<double> directions(3);
+    std::vector<double> directions(3); // [0]=left [1]=middle [2]=right
     std::vector<int> values(3);
 
-    //region * Get directions and values in them *
+    // region data
+    // get the three directions agent can look at and the values of the cells at these 3 positions
     for (int i = -1; i <= 1; ++i) {
         directions[i + 1] = agent->angle + i * ANGLE_OF_VIEW;
 
+        // get x position and if dangerous, wrap around screen
         double new_x = agent->pos.x + DISTANCE_FROM_AGENT * std::cos(directions[i + 1]);
         if (new_x < 0) {
             new_x = WIDTH + new_x;
@@ -139,6 +142,7 @@ void AgentProcessor::updateAgentWrapping(Agent *agent) {
             new_x -= WIDTH;
         }
 
+        // get y position and if dangerous, wrap around screen
         double new_y = agent->pos.y + DISTANCE_FROM_AGENT * std::sin(directions[i + 1]);
         if (new_y < 0) {
             new_y = HEIGHT + new_y;
@@ -150,7 +154,7 @@ void AgentProcessor::updateAgentWrapping(Agent *agent) {
     }
     //endregion
 
-    //region * Update agent angle *
+    //region Update agent angle
     if (values[0] > values[2]) {
         agent->angle = directions[0];
     } else if (values[2] > values[0]) {
@@ -162,7 +166,7 @@ void AgentProcessor::updateAgentWrapping(Agent *agent) {
     }
     //endregion
 
-    //region * Update position *
+    //region Update position
     float x_move = std::cos(agent->angle);
     float y_move = std::sin(agent->angle);
     agent->pos.x += (x_move < 0) ? -1 : (x_move == 0) ? 0 : 1;
